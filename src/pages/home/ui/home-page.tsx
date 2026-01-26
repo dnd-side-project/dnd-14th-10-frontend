25a2b6c933b807638dd6daca4dc3cdefdbdad9a9
callmejeje <choije401@naver.com>
2026-01-26 17:57:12 +0900

    chore: 라우팅 설정

---
+++ src/pages/home/ui/home-page.tsx
@@ -0,0 +1,5 @@
+function HomePage() {
+  return <div>home입니다.</div>;
+}
+
+export default HomePage;
---
+++ src/pages/login/ui/login-page.tsx
@@ -0,0 +1,5 @@
+function LoginPage() {
+  return <div>login입니다.</div>;
+}
+
+export default LoginPage;
