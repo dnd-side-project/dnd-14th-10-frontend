17e69b24b16c229d5f85e1cc9d1d91bdd59f617b
callmejeje <choije401@naver.com>
2026-01-29 16:51:20 +0900

    chore: 페이지 라우팅 추가

--- src/app/providers/router.tsx
+++ src/app/providers/router.tsx
@@ -2,6 +2,11 @@ import { createBrowserRouter } from 'react-router-dom';
 
 import HomePage from '@/pages/home/ui/home-page';
 import LoginPage from '@/pages/login/ui/login-page';
+import MapPage from '@/pages/map/ui/map-page';
+import MyPage from '@/pages/my/ui/my-page';
+import RegistrationPage from '@/pages/registration/ui/registration-page';
+import ReviewCreationPage from '@/pages/review-creation/ui/review-creation-page';
+import SpaceDetailPage from '@/pages/space-detail/ui/space-detail-page';
 
 export const router = createBrowserRouter([
   {
@@ -11,5 +16,25 @@ export const router = createBrowserRouter([
   {
     path: '/login',
     element: <LoginPage />,
+  },
+  {
+    path: '/map',
+    element: <MapPage />,
+  },
+  {
+    path: '/my',
+    element: <MyPage />,
+  },
+  {
+    path: '/registration',
+    element: <RegistrationPage />,
+  },
+  {
+    path: '/space/:id',
+    element: <SpaceDetailPage />,
+  },
+  {
+    path: '/review-creation/:id',
+    element: <ReviewCreationPage />,
   },
 ]);
