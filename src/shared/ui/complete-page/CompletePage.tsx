004ee82c910438b88a207ae2d8754ab2ea6e9c88
callmejeje <choije401@naver.com>
2026-02-24 21:08:22 +0900

    feat: 리뷰 작성 api 연결

--- src/shared/ui/complete-page/CompletePage.tsx
+++ src/shared/ui/complete-page/CompletePage.tsx
@@ -3,7 +3,8 @@ import NavigationBar from '@/shared/ui/navigation-bar/NavigationBar';
 
 interface CompletePageProps {
   title: string;
-  imageSrc: string;
+  imageSrc?: string;
+  fallbackImage?: string;
   name: string;
   description?: string;
   buttonLabel: string;
@@ -14,6 +15,7 @@ interface CompletePageProps {
 export function CompletePage({
   title,
   imageSrc,
+  fallbackImage,
   name,
   description,
   buttonLabel,
@@ -32,24 +34,30 @@ export function CompletePage({
       />
       <div className='flex h-[calc(100vh-48px)] flex-col items-center justify-center bg-white'>
         <div className='flex flex-col items-center gap-[60px]'>
-          <div className='text-center text-2xl leading-tight font-bold text-black'>{title}</div>
-          <div className='flex flex-col items-center gap-6'>
-            <img
-              className='h-60 w-60 rounded-xl border border-gray-300'
-              src={imageSrc}
-              alt={name}
-            />
-            <div className='flex flex-col items-center justify-center gap-2'>
-              <div className='text-center text-lg leading-normal font-medium text-gray-950'>
-                {name}
-              </div>
-              {description && (
-                <div className='text-center text-lg leading-normal font-normal text-gray-500'>
-                  {description}
-                </div>
-              )}
-            </div>
+          <div className='text-center text-2xl leading-tight font-bold whitespace-pre-line text-black'>
+            {title}
           </div>
+          {imageSrc ? (
+            <div className='flex flex-col items-center gap-6'>
+              <img
+                className='h-60 w-60 rounded-xl border border-gray-300'
+                src={imageSrc}
+                alt={name}
+              />
+              <div className='flex flex-col items-center justify-center gap-2'>
+                <div className='text-center text-lg leading-normal font-medium text-gray-950'>
+                  {name}
+                </div>
+                {description && (
+                  <div className='text-center text-lg leading-normal font-normal text-gray-500'>
+                    {description}
+                  </div>
+                )}
+              </div>
+            </div>
+          ) : (
+            fallbackImage && <img src={fallbackImage} alt='완료' className='w-[140px]' />
+          )}
         </div>
         <button
           type='button'
