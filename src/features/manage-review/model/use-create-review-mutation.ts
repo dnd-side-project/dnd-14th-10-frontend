5fddb67c17aa6b58a3ba772b6ea536cd084fa989
callmejeje <choije401@naver.com>
2026-02-24 20:38:21 +0900

    feat: 리뷰 등록 타입 맞추기

--- src/features/manage-review/model/use-create-review-mutation.ts
+++ src/features/manage-review/model/use-create-review-mutation.ts
@@ -1,10 +1,10 @@
 import { useMutation } from '@tanstack/react-query';
 
+import type { CreateReviewRequest } from '../api/review.api';
 import { createReview } from '../api/review.api';
 
 export const useCreateReviewMutation = () => {
   return useMutation({
-    mutationFn: ({ placeId, data }: { placeId: string; data: unknown }) =>
-      createReview(placeId, data),
+    mutationFn: (data: CreateReviewRequest) => createReview(data),
   });
 };
